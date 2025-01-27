df504d950376ee1d04df30e72acd1b4c86bae209
gnjrk24 <gnjrk24@gmail.com>
2025-01-27 23:51:33 +0900

    html-vol.2

---
+++ js-easy/js-basic/main.js
@@ -0,0 +1,16 @@
+console.log(`hello,world!`)
+console.log("こんにちは世界！")
+console.log(1 + 2)
+console.log(10 - 4 * 8)
+const answer = 42
+console.log(answer)
+
+const breakfast = "納豆ごはん";
+console.log(`今日の朝ごはんは${breakfast}でした。`);
+
+
+//この条件式はどちらになるでしょう。 3 > 7 || 2 < 9 
+
+
+// このnumObjの2にアクセス(取得)したいとき  答えnumobj["b"]
+const numObj = { a: 1, b: 2, c: 3, d: 4, e: 5 };
