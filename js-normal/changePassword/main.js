20dc53685f74fceac0a12209cb3bb9a376dd07ad
gnjrk24 <gnjrk24@gmail.com>
2025-02-13 01:20:17 +0900

    password

--- js-normal/changePassword/main.js
+++ js-normal/changePassword/main.js
@@ -1,12 +1,8 @@
 //現在のパスワード入力
 //新しいパスワードを入力
 
-//現在のパスワードの一致、旧・新パスワードが異なる
-//条件を満たしたらパスワード変更される
-//条件が合わない場合はアラート
-
-const now = document.getElementById('nowPassword');
-let pass = 'aaaaa'
+const now = document.getElementById("nowPassword");
+let pass = "aaaaa";
 now.textContent = `現在のパスワードは ${pass}です`;
 
 const old = document.getElementById("confirmPassword");
@@ -14,10 +10,15 @@ const newPass = document.getElementById("newPassword");
 
 //変更ボタンを押す
 const setBtn = document.getElementById("setPassword");
-setBtn.addEventListener('click', function() {
-
-
-   if (pass === old) {
-
-   }
+setBtn.addEventListener("click", function () {
+  if (pass !== old) {
+    alert("パスワードが違います");
+    return;
+  }
+  if (old === newPass) {
+    alert("同じパスワードは使えません");
+    return;
+  }
+  //パスワード変更
+  now.textContent = `現在のパスワードは ${pass}です`;
 });
--- js-normal/evolution/main.js
+++ js-normal/evolution/main.js
@@ -2,37 +2,43 @@ const btn = document.getElementById("setBtn");
 const reset = document.getElementById("resetBtn");
 const showImg = document.getElementById("showImg");
 
-
-//genjin.innerHTML = "<img src='./img/evolution1.png' />";
-const genjin =  document.createElement('img');
-genjin.setAttribute('src', './img/evolution1.png');
+// 4/10 の確率で1枚の画像
+const genjin = document.createElement("img");
+genjin.setAttribute("src", "./img/evolution1.png");
+showImg.appendChild(genjin)
 
 //ボタンを押す
 btn.addEventListener("click", function () {
-    const evo = Math.floor(Math.random() * 10);
-    // 4/10 の確率で1枚の画像 
+  const evo = Math.floor(Math.random() * 10 + 1);
+  //console.log(evo);
+  //console.log(1);
 
-    
+  if (evo >= 5) {
     // 3/10 の確率で2枚の画像
-    if( evo >= 4){
-
-    }
+    const kyujin = document.createElement("img");
+    kyujin.setAttribute("src", "./img/evolution2.png");
+    showImg.appendChild(kyujin);
+    //console.log(2);
+  }
+ 
+  if (evo >= 8) {
     // 2/10 の確率で3枚の画像
-    if( evo >= 7){
-
-    }
+    const shinjin = document.createElement("img");
+    shinjin.setAttribute("src", "./img/evolution3.png");
+    showImg.appendChild(shinjin);
+    //console.log(3);
+  }
+ 
+  if (evo > 9) {
     // 1/10 の確率で4枚の画像
-    if( evo >= 9){
-
-    }
+    const hito = document.createElement("img");
+    hito.setAttribute("src", "./img/evolution4.png");
+    showImg.appendChild(hito);
+    //onsole.log(4);
+  }
 });
-
-<img src = './img/evolution1.png'/>
-<img src = './img/evolution2.png'/>
-<img src = './img/evolution3.png'/>
-<img src = './img/evolution4.png'/>
 
 //リセットボタンで一括削除
 reset.addEventListener("click", function () {
-
+showImg.innerHTML = "";
 });
