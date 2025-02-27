600d9142f8cc80eaebe0b92d933cdb4c2808a662
gnjrk24 <gnjrk24@gmail.com>
2025-02-27 18:01:56 +0900

    227

--- js-hard/howmanydays/main.js
+++ js-hard/howmanydays/main.js
@@ -1,31 +1,28 @@
-const now = moment();
-console.log(now.format('YYYY年MM年DD日'));
+const display = document.createElement("p")//<p></p>
+body= document.querySelector("body")   
+body.appendChild(display) //<body><p>  </p></body>
 
-//setInterval(countdown(){}, 1000);
-    // 現在時刻
-    const day1= new Date();
-    //body.appendChild(now);
-    day1 = day1.getTime();
-    console.log(day1);
-    //指定した日時
-    const day2 = new Date("2222/2/22"); 
-    day2 = day2.getTime();
-    console.log(day2);
+   
+    let now = moment('2025-01-1 00:00:00');
+    console.log(now);
+
+    let day = moment('2026-01-1 10:10:10');
+    console.log(day); 
+    //day-now
+    let b = day.diff(now);
+    console.log(b);  
+    
+    let d = Math.floor(day.diff(now) / (24 * 60 * 60 * 1000));//日
+    let h = Math.floor(day.diff(now) / (60 * 60 * 1000))% 60; //時間
+    let m = Math.floor(day.diff(now)/ (60 * 1000))% 60;//分
+    let s = Math.floor(day.diff(now) / 1000)% 60; //秒
+        console.log(d);
+        console.log(h);
+        console.log(m);
+        console.log(s);
+    display.textContent = `ドラえもんが生まれるまで後${d}日${h}時間${m}分${s}秒`;
+  
 
 
-    //<p></p>
-    const display = document.createElement("p");
-    body= document.querySelector("body");
-    //<body><p>  </p></body>
-    body.appendChild(display);
-
-    d = ((day2 - day1) / (24 * 60 * 60 * 1000));//日
-    h = ((day2 - day1) / (60 * 60 * 1000)); //時間
-    m = ((day2 - day1) / (60 * 1000));//分
-    s = ((day2 - day1) / 1000); //秒
-
-    display.textContent = `ドラえもんが生まれるまで後${d}日${h}時間${m}分${s}秒｀;
-
-    //1 秒ごとに更新する
-
+//setInterval(countDown, 1000);
 
--- js-hard/numeron/main.js
+++ js-hard/numeron/main.js
@@ -1,2 +1,14 @@
+const x = document.getElementById("answerNum");
+const button = document.getElementById("numCheck");
 
+//3桁の数字を決める
+
+let leftNum = Math.floor(Math.random()*10);
+let cNum = Math.floor(Math.random()*10);
+let rightNum = Math.floor(Math.random()*10);
+//数字を入力
+//ボタン：答え合わせ button.addEventListenner('click', function(){};
+//数字と桁が合っていたら「EAT」、数字はあっていて桁が違っているときは「BITE」
+//アラート alert(' ETA, BITE');
+// 正解 alert('正解です！');
 
